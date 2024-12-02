ca82128c89c657eaf7c321b3cc497c9df54cf668
seongmin <min@seongminui-MacBookPro.local>
2024-12-02 22:08:19 +0900

    스프레드시트에 모델 적용

--- script.js
+++ script.js
@@ -2,20 +2,6 @@ const ROWS = 10;
 const COLS = 10;
 const spreadsheet = [];
 
-// 기본 데이터 생성
-initSpreadsheet();
-
-function initSpreadsheet() {
-	for (let i = 0; i < ROWS; i++) {
-		let spreadsheetRow = [];
-		for (let j = 0; j < COLS; j++) {
-			spreadsheetRow.push(i + '-' + j);
-		}
-		spreadsheet.push(spreadsheetRow);
-	}
-	console.log(spreadsheet);
-}
-
 class Cell {
 	constructor(isHeader, disabled, data, row, column, active = false) {
 		this.isHeader = isHeader;
@@ -25,4 +11,19 @@ class Cell {
 		this.column = column;
 		this.active = active;
 	}
+}
+
+// 기본 데이터 생성
+initSpreadsheet();
+
+function initSpreadsheet() {
+	for (let i = 0; i < ROWS; i++) {
+		let spreadsheetRow = [];
+		for (let j = 0; j < COLS; j++) {
+			const cell = new Cell(false, false, i + '-' + j, i, j, false);
+			spreadsheetRow.push(cell);
+		}
+		spreadsheet.push(spreadsheetRow);
+	}
+	console.log(spreadsheet);
 }
