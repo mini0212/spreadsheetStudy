4c1b93ce3b02498b475b52fd7da92770435087eb
seongmin <min@seongminui-MacBookPro.local>
2024-12-02 22:42:51 +0900

    csv 파일 생성

--- script.js
+++ script.js
@@ -176,5 +176,13 @@ function clearHeaderActiveStatus() {
 const exportButton = document.querySelector('#export-button');
 
 exportButton.addEventListener('click', (event) => {
-	console.log(spreadsheet);
+	let csv = '';
+	for (i = 0; i < spreadsheet.length; i++) {
+		csv +=
+			spreadsheet[i]
+				.filter((item) => !item.isHeader)
+				.map((item) => item.data)
+				.join(',') + '\r\n';
+	}
+	console.log(csv);
 });
