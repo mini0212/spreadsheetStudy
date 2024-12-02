72a93673ebb9dfebf7024c7b74408df00b322dec
seongmin <min@seongminui-MacBookPro.local>
2024-12-02 22:06:56 +0900

    스프레트 시트 셀 모델 생성

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function initSpreadsheet() {
 	}
 	console.log(spreadsheet);
 }
+
+class Cell {
+	constructor(isHeader, disabled, data, row, column, active = false) {
+		this.isHeader = isHeader;
+		this.disabled = disabled;
+		this.data = data;
+		this.row = row;
+		this.column = column;
+		this.active = active;
+	}
 }
